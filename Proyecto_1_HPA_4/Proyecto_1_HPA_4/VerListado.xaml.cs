8d06d7f3404538831cdf6ac3d51ce339f0348711
Eric Mason <57864479+ericMaxon@users.noreply.github.com>
2022-10-17 22:40:03 -0500

    listview

--- Proyecto_1_HPA_4/Proyecto_1_HPA_4/VerListado.xaml.cs
+++ Proyecto_1_HPA_4/Proyecto_1_HPA_4/VerListado.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Proyecto_1_HPA_4.modelos;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,20 +15,18 @@ namespace Proyecto_1_HPA_4
     {
         public VerListado()
         {
+            ObservableCollection<Estudiante> estudiantes = new ObservableCollection<Estudiante>
+            {
+                new Estudiante
+                {
+                    Nombre="Pepe",
+                    Cedula="1-1234-123123",
+                    Fecha=DateTime.Now.ToString()
+                }
+            };
             InitializeComponent();
             var listView = new ListView();
-            listView.ItemsSource = new string[]
-            {
-                  "mono",
-                  "monodroid",
-                  "monotouch",
-                  "monorail",
-                  "monodevelop",
-                  "monotone",
-                  "monopoly",
-                  "monomodal",
-                  "mononucleosis"
-            };
+            listView.ItemsSource = estudiantes;
         }
     }
 }
